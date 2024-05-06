4d4a44117ab0536d9631ad7a1d3471611b8c9c93
Sushant-Ohlyan <sushantohlyan100@gmail.com>
2024-05-06 23:52:41 +0530

    Program 3

---
+++ Program_3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 6;
+    int c = a + b;
+    printf("%d", c); // Corrected usage of printf with a format specifier %d for integer
+    return 0;
+}
